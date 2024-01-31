2b053786c3ee99e135bf0bd8d04c480815173bde
caminobelllo <alstpgml0812@gmail.com>
2024-02-01 00:11:01 +0900

    로그아웃 기능 구현

--- homepage/src/pages/kahlua_admin/AdminHeader.tsx
+++ homepage/src/pages/kahlua_admin/AdminHeader.tsx
@@ -1,11 +1,29 @@
 import Image from "next/image";
 import React from "react";
 import logo from "/public/assets/images/admin/admin_logo.svg";
+import { logout } from "@/apis/logout";
+import { Router, useRouter } from "next/router";
 
 const AdminHeader = () => {
+  const router = useRouter();
+
+  const onLogoutHandler = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    router.push("/login");
+  };
+
   return (
-    <div className="w-full bg-white ml-[166px] mt-[28px] mb-[26px]">
+    <div className="w-full bg-white pl-[166px] mt-[28px] mb-[26px] flex justify-between">
       <Image src={logo} alt="admin_logo" height={26} />
+      <button
+        className="mr-[180px] rounded-[10px] bg-black text-white px-[12px] py-[4px]"
+        onClick={onLogoutHandler}
+      >
+        <p>로그아웃</p>
+      </button>
     </div>
   );
 };
--- homepage/src/pages/kahlua_admin/index.tsx
+++ homepage/src/pages/kahlua_admin/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AdminHeader from "./AdminHeader";
 import { useState } from "react";
 import DropdownApplication from "./DropdownApplication";
