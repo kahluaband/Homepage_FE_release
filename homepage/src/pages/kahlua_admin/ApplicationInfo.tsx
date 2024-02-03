aac2314e533f5305658acc6cc25765dbdace1609
limgahyun <limgh2002@gmail.com>
2024-02-04 06:04:54 +0900

    Feat: sampleApplication 확인

--- homepage/src/pages/kahlua_admin/ApplicationInfo.tsx
+++ homepage/src/pages/kahlua_admin/ApplicationInfo.tsx
@@ -1,54 +1,51 @@
-const AppItem = (application: any) => {
-    const {key, st_name, gender, birthdate, phone_num, major, address, first_preference, second_preference, reason, motive, instrument, finish_time, meeting, readiness} = application
+const AppItem: React.FC<{ application: any }> = ({application}) => {
+    const {name, gender, birthdate, phone_num, major, address, first_preference, second_preference, experience_and_reason, motive, play_instrument, finish_time, meeting, readiness} = application;
+    
     return(
-    <div className="whitespace-nowrap overflow-auto flex flex-col">
-      {<>
-        <li className="flex flex-row h-16 w-[1712px] px-4 items-center text-center" key={key}>
+        <li className="flex flex-row h-16 w-[1712px] px-4 items-center text-center">
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {st_name}
+                {name}
             </p>
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {gender}
+                {gender}
             </p>
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {birthdate}
+                {birthdate}
             </p>
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {phone_num}
+                {phone_num}
             </p>
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {major}
+                {major}
             </p>
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {address}
+                {address}
             </p>
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {first_preference}
+                {first_preference}
             </p>
             <p className="flex justify-center items-center w-[100px] h-full text-base font-bold p-2">
-            {second_preference}
+                {second_preference}
             </p>
             <p className="flex justify-center items-center w-[220px] h-full text-base font-bold p-2">
-            {reason}
+                {experience_and_reason}
             </p>
             <p className="flex justify-center items-center w-[140px] h-full text-base font-bold p-2">
-            {motive}
+                {motive}
             </p>
             <p className="flex justify-center items-center w-[140px] h-full text-base font-bold p-2">
-            {instrument}
+                {play_instrument}
             </p>
             <p className="flex justify-center items-center w-[140px] h-full text-base font-bold p-2">
-            {finish_time}
+                {finish_time}
             </p>
             <p className="flex justify-center items-center w-[120px] h-full text-base font-bold p-2">
-            {meeting}
+                {meeting}
             </p>
             <p className="flex justify-center items-center w-[120px] h-full text-base font-bold p-2">
-            {readiness}
+                {readiness}
             </p>
         </li>
-      </>}
-    </div>
     )
 }
 
--- homepage/src/pages/kahlua_admin/ApplicationList.tsx
+++ homepage/src/pages/kahlua_admin/ApplicationList.tsx
@@ -27,43 +27,107 @@ const sampleApplication = {
 
 const AppList = () => {
 
-    const access = localStorage.getItem("access");
-    const authAxios = getAuthAxios(access);
-    const [applications, setApplications] = useState<any[]>([]);
-    const [loading, setLoading] = useState(false);
+    // const access = localStorage.getItem("access");
+    // const authAxios = getAuthAxios(access);
+    // const [applications, setApplications] = useState<any[]>([]);
+    // const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const response = await authAxios.get(
-                    `https://api.kahluaband.com/kahlua_admin/application/apply_forms/`,{
-                    }
-                );
-                setApplications(response.data.apply_forms)
-                console.log(applications);
-            } catch(error){
-                console.log(error);
-            }
-            setLoading(false);
-        }
-        fetchData();
-    },[]);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         setLoading(true);
+    //         try {
+    //             const response = await authAxios.get(
+    //                 `https://api.kahluaband.com/kahlua_admin/application/apply_forms/`,{
+    //                 }
+    //             );
+    //             setApplications(response.data.apply_forms)
+    //             console.log(applications);
+    //         } catch(error){
+    //             console.log(error);
+    //         }
+    //         setLoading(false);
+    //     }
+    //     fetchData();
+    // },[]);
 
-    if (loading) {
-        return <div>대기 중 ...</div>;
-    }
+    // if (loading) {
+    //     return <div>대기 중 ...</div>;
+    // }
 
-    if (!applications) {
-        return null;
-    }
+    // if (!applications) {
+    //     return null;
+    // }
+
+    // return (
+    //     <div>
+    //         {applications.map((application) => (
+    //             <AppItem key={application.id} application={application}/>
+    //         ))}
+    //     </div>
+    // );
 
     return (
-        <div>
-            {applications.map((application) => (
-                <AppItem key={application.id} application={application}/>
-            ))}
+
+        <div className="whitespace-nowrap overflow-auto flex flex-col">
+            {<>
+                <div className="flex flex-row w-full px-10 py-4 gap-4">
+                    <p className="flex justify-center items-center w-[140px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
+                        23기 지원자 정보
+                    </p>
+                    <p className="flex justify-center items-center w-[80px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
+                        ?명
+                    </p>
+                </div>
+
+                <li className="flex flex-row h-16 w-[1712px] bg-[#D9D9D9] px-4 items-center text-center">
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        이름
+                    </p>
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        성별
+                    </p>
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        생년월일
+                    </p>
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        전화번호
+                    </p>
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        학과
+                    </p>
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        거주지
+                    </p>
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        1지망 선택
+                    </p>
+                    <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        2지망 선택
+                    </p>
+                    <p className="flex justify-center items-center w-[220px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        지원 세션의 경력과 지원 이유
+                    </p>
+                    <p className="flex justify-center items-center w-[140px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        깔루아 지원 동기
+                    </p>
+                    <p className="flex justify-center items-center w-[140px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        다룰 줄 아는 악기
+                    </p>
+                    <p className="flex justify-center items-center w-[140px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        수업 끝나는 시간
+                    </p>
+                    <p className="flex justify-center items-center w-[120px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        뒷풀이 참여
+                    </p>
+                    <p className="flex justify-center items-center w-[120px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+                        각오 한마디
+                    </p>
+                </li>
+                
+                <AppItem application={sampleApplication}/>
+            </>}
         </div>
+
     );
 }
 
