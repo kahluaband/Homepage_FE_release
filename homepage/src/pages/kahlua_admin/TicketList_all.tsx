45d569551e3b3de196b6b542d0e84da39fce0b8d
caminobelllo <alstpgml0812@gmail.com>
2024-02-29 00:56:38 +0900

    feat: 어드민 withCredentials 설정 추가

--- homepage/src/pages/kahlua_admin/TicketList_all.tsx
+++ homepage/src/pages/kahlua_admin/TicketList_all.tsx
@@ -4,66 +4,66 @@ import axios from "axios";
 import { useRouter } from "next/router";
 
 const AllTicketList = () => {
-
   const router = useRouter();
   const [name, setName] = useState("");
 
   useEffect(() => {
     const fetchReservationData = async () => {
-        try {
-          const response = await axios.get(
-            `https://api.kahluaband.com/kahlua_admin/tickets/all/`,
-          );
-          if (response.status === 200) {
-            setName(response.data.data.name);
-            
-          } 
-          else {
-          }
-        } catch (error) {}
-      };
+      try {
+        const response = await axios.get(
+          `https://api.kahluaband.com/kahlua_admin/tickets/all/`,
+          { withCredentials: true }
+        );
+        if (response.status === 200) {
+          setName(response.data.data.name);
+        } else {
+        }
+      } catch (error) {}
+    };
     fetchReservationData();
-  }, );
+  });
 
   return (
     <div className="whitespace-nowrap overflow-auto flex flex-col">
-      {<>
-      <div className="flex flex-row w-full px-10 py-4 gap-4">
-        <p className="flex justify-center items-center w-[140px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
-          2024.03 정기공연
-        </p>
-        <p className="flex justify-center items-center w-[80px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
-          ?명
-        </p>
-      </div>
+      {
+        <>
+          <div className="flex flex-row w-full px-10 py-4 gap-4">
+            <p className="flex justify-center items-center w-[140px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
+              2024.03 정기공연
+            </p>
+            <p className="flex justify-center items-center w-[80px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
+              ?명
+            </p>
+          </div>
 
-      <div className="flex flex-row h-16 w-[1712px] bg-[#D9D9D9] px-4 items-center text-center">
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          예매번호
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          이름
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          전화번호
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          매수
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          학과
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          학번
-        </li>
-        <li className="flex justify-center items-center w-[120px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          뒷풀이 참석 여부
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          결제상황
-        </li>
-      </div>
-      </>}
+          <div className="flex flex-row h-16 w-[1712px] bg-[#D9D9D9] px-4 items-center text-center">
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              예매번호
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              이름
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              전화번호
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              매수
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              학과
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              학번
+            </li>
+            <li className="flex justify-center items-center w-[120px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              뒷풀이 참석 여부
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              결제상황
+            </li>
+          </div>
+        </>
+      }
     </div>
   );
 };
--- homepage/src/pages/kahlua_admin/TicketList_freshman.tsx
+++ homepage/src/pages/kahlua_admin/TicketList_freshman.tsx
@@ -4,9 +4,8 @@ import axios from "axios";
 import { useRouter } from "next/router";
 
 const FreshmanTicketList = () => {
-
   const router = useRouter();
-  
+
   const [buyer, setBuyer] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
   const [major, setMajor] = useState("");
@@ -15,54 +14,56 @@ const FreshmanTicketList = () => {
 
   useEffect(() => {
     const fetchReservationData = async () => {
-        try {
-          const response = await axios.get(
-            `https://api.kahluaband.com/kahlua_admin/tickets/freshman_tickets/`,
-          );
-          if (response.status === 200) {
-            setBuyer(response.data.tickets.buyer);
-            setPhoneNum(response.data.tickets.phone_num);            
-          } 
-          else {
-          }
-        } catch (error) {}
-      };
+      try {
+        const response = await axios.get(
+          `https://api.kahluaband.com/kahlua_admin/tickets/freshman_tickets/`,
+          { withCredentials: true }
+        );
+        if (response.status === 200) {
+          setBuyer(response.data.tickets.buyer);
+          setPhoneNum(response.data.tickets.phone_num);
+        } else {
+        }
+      } catch (error) {}
+    };
     fetchReservationData();
-  }, );
+  });
 
   return (
     <div className="whitespace-nowrap overflow-auto flex flex-col">
-      {<>
-      <div className="flex flex-row w-full px-10 py-4 gap-4">
-        <p className="flex justify-center items-center w-[140px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
-          2024.03 정기공연
-        </p>
-        <p className="flex justify-center items-center w-[80px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
-          ?명
-        </p>
-      </div>
+      {
+        <>
+          <div className="flex flex-row w-full px-10 py-4 gap-4">
+            <p className="flex justify-center items-center w-[140px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
+              2024.03 정기공연
+            </p>
+            <p className="flex justify-center items-center w-[80px] h-8 rounded-[24px] bg-[#D9D9D9] text-center text-base text-black">
+              ?명
+            </p>
+          </div>
 
-      <div className="flex flex-row h-16 w-[1712px] bg-[#D9D9D9] px-4 items-center text-center">
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          예매번호
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          이름
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          전화번호
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          학과
-        </li>
-        <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          학번
-        </li>
-        <li className="flex justify-center items-center w-[120px] h-full bg-[#D9D9D9] text-base font-bold p-2">
-          뒷풀이 참석 여부
-        </li>
-      </div>
-      </>}
+          <div className="flex flex-row h-16 w-[1712px] bg-[#D9D9D9] px-4 items-center text-center">
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              예매번호
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              이름
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              전화번호
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              학과
+            </li>
+            <li className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              학번
+            </li>
+            <li className="flex justify-center items-center w-[120px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+              뒷풀이 참석 여부
+            </li>
+          </div>
+        </>
+      }
     </div>
   );
 };
