9440af84684f3e233f577498a11574662621f946
limgahyun <limgh2002@gmail.com>
2024-03-01 03:52:06 +0900

    Design: application list width수정

--- homepage/src/app/components/kahlua_admin/ApplicationList.tsx
+++ homepage/src/app/components/kahlua_admin/ApplicationList.tsx
@@ -78,7 +78,7 @@ const AppList: React.FC<AppListProps> = ({session}) => {
             </p>
           </div>
 
-          <li className="flex flex-row h-16 w-[3292px] bg-[#D9D9D9] px-4 items-center text-center">
+          <li className="flex flex-row h-16 w-[2692px] bg-[#D9D9D9] px-4 items-center text-center">
             <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
               이름
             </p>
@@ -103,13 +103,13 @@ const AppList: React.FC<AppListProps> = ({session}) => {
             <p className="flex justify-center items-center w-[100px] h-full bg-[#D9D9D9] text-base font-bold p-2">
               2지망 선택
             </p>
-            <p className="flex justify-center items-center w-[600px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+            <p className="flex justify-center items-center w-[400px] h-full bg-[#D9D9D9] text-base font-bold p-2">
               지원 세션의 경력과 지원 이유
             </p>
-            <p className="flex justify-center items-center w-[600px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+            <p className="flex justify-center items-center w-[400px] h-full bg-[#D9D9D9] text-base font-bold p-2">
               깔루아 지원 동기
             </p>
-            <p className="flex justify-center items-center w-[600px] h-full bg-[#D9D9D9] text-base font-bold p-2">
+            <p className="flex justify-center items-center w-[400px] h-full bg-[#D9D9D9] text-base font-bold p-2">
               다룰 줄 아는 악기
             </p>
             <p className="flex justify-center items-center w-[140px] h-full bg-[#D9D9D9] text-base font-bold p-2">
